ae6e7d551f8a073a01ddafb16fa7ac1d6775a2ef
gagarin <gagarinbk@gmail.com>
2023-04-15 21:17:42 +0300

    Added Food Page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HomeComponent } from './components/pages/home/home.component';
 import { RatingModule } from 'ng-starrating';
 import { SearchComponent } from './components/partials/search/search.component';
 import { TagsComponent } from './components/partials/tags/tags.component';
+import { FoodPageComponent } from './components/pages/food-page/food-page.component';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { TagsComponent } from './components/partials/tags/tags.component';
     HomeComponent,
     SearchComponent,
     TagsComponent,
+    FoodPageComponent,
   ],
   imports: [
     BrowserModule,
